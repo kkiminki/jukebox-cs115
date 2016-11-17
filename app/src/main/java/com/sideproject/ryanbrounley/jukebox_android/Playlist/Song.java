d4a2faf03e8db1b937defa1dabc12eff4b7166ad
Kyler Kiminki <kkiminki@ucsc.edu>
2016-11-17 13:17:23 -0800

    Added comments to song

--- app/src/main/java/com/sideproject/ryanbrounley/jukebox_android/Playlist/Song.java
+++ app/src/main/java/com/sideproject/ryanbrounley/jukebox_android/Playlist/Song.java
@@ -14,11 +14,8 @@ public class Song {
     private URL thumbnail;
     private String name;
     private int upvotes=0;
-    private int downvotes=0;
 
-    //public Song(){
-    //}
-
+    //Constructor for the song class
     public Song(String artist, String uri, String album, URL image, URL thumbnail, String name){
         this.artist = artist;
         this.uri = uri;
@@ -28,75 +25,90 @@ public class Song {
         this.name = name;
     }
 
+    //Constructor for testing
+    //TODO Delete this constructor
     public Song(String uri){
         this.uri=uri;
     }
 
+    //TODO Delete setters: should be set by constructor
+    //Artist setter
     public void setArtist(String artist){
         this.artist = artist;
     }
 
+    //Artist getter
     public String getArtist(){
         return this.artist;
     }
 
+    //URI setter
     public void setUri(String uri){
         this.uri = uri;
     }
 
+    //URI getter
     public String getUri(){
         return this.uri;
     }
 
+    //Album setter
     public void setAlbum(String album){
         this.album = album;
     }
 
+    //Album getter
     public String getAlbum(){
         return this.album;
     }
 
+    //Image setter
     public void setImage(URL image){
         this.image = image;
     }
 
+    //Image getter
     public URL getImage(){
         return this.image;
     }
 
+    //Thumbnail setter
     public void setThumbnail(URL thumbnail){
         this.thumbnail = thumbnail;
     }
 
+    //Thumbnail getter
     public URL getThumbnail(){
         return this.thumbnail;
     }
 
+    //Name setter
     public void setName(String name){
         this.name = name;
     }
 
+    //Name getter
     public String getName(){
         return this.name;
     }
 
+    //Equals comparison method
     public boolean equals(Song s){
         return this.uri == s.uri;
     }
 
+    //Upvote incrementer
     public void addUpvote(){
         this.upvotes++;
     }
 
+    //Upvotes getter
     public int getUpvotes(){
         return this.upvotes;
     }
 
+    //Upvotes decrementer
     public void addDownvote(){
-        this.downvotes++;
-    }
-
-    public int getDownvotes(){
-        return this.downvotes;
+        this.upvotes--;
     }
 }
