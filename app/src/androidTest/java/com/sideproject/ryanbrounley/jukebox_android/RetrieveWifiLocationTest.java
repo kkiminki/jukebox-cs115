221e431478752bcd1e5a83b91d085ef2db8887ef
Matthew Mayr <mmmayr@ucsc.edu>
2016-11-20 13:19:39 -0800

    Added and ran tests; all passed.

--- app/src/androidTest/java/com/sideproject/ryanbrounley/jukebox_android/RetrieveWifiLocationTest.java
+++ app/src/androidTest/java/com/sideproject/ryanbrounley/jukebox_android/RetrieveWifiLocationTest.java
@@ -1,5 +1,8 @@
 package com.sideproject.ryanbrounley.jukebox_android;
 
+import android.content.Context;
+import android.location.LocationManager;
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -8,9 +11,12 @@ import static org.junit.Assert.*;
  * Created by Dell on 11/14/2016.
  */
 public class RetrieveWifiLocationTest {
+    Context tContext;
+    private RetrieveWifiLocation rwl = new RetrieveWifiLocation(tContext);
     @Test
-    public void getLocation() throws Exception {
-
-    }
+        public void testNullLm () {
+            rwl.getLocation();
+            assertNotNull(rwl.lm);
+        }
 
 }
