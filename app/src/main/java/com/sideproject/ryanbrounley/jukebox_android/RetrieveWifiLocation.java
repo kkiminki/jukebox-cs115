e7b695bfaed027ad390cdf99e8f0d2f3c181542b
Matthew Mayr <mmmayr@ucsc.edu>
2016-11-20 13:15:24 -0800

    getLocation() returns an LocationManager object.  No errors for self-sustaining file, but may need to adjust access for the larger project.

--- app/src/main/java/com/sideproject/ryanbrounley/jukebox_android/RetrieveWifiLocation.java
+++ app/src/main/java/com/sideproject/ryanbrounley/jukebox_android/RetrieveWifiLocation.java
@@ -8,25 +8,29 @@ import android.util.Log;
  * Committed by Matthew Mayr on 11/3/2016.
  */
 
-public class RetrieveWifiLocation extends FakeFragment {
+class RetrieveWifiLocation {
 
-    Context lContext;
-    public RetrieveWifiLocation(Context lContext) {
+    private Context lContext;
+    RetrieveWifiLocation(Context lContext) {
         this.lContext = lContext;
     }
+    LocationManager lm = (LocationManager) lContext.getSystemService(Context.LOCATION_SERVICE);
 
-    LocationManager lm=(LocationManager) lContext.getSystemService(Context.LOCATION_SERVICE);
-    boolean wifiEnabled = lm
-            .isProviderEnabled(LocationManager.NETWORK_PROVIDER);
-
-    public void getLocation() {
-        //gets rough location from wifi name every 10 seconds or 10 meters
-        if (wifiEnabled) {
-            lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 10000L, 10F, (android.location.LocationListener) this);
+    LocationManager getLocation() {
+        if (lm != null) {
+            boolean wifiEnabled = lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+            //gets rough location from wifi name every 10 seconds or 10 meters
+            if (wifiEnabled) {
+                lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 10000L, 10F, (android.location.LocationListener) this);
+                return lm;
+            } else {
+                wifiError();
+            }
         } else {
-            wifiError();
+            System.out.println ("Something went wrong with location manager.");
         }
+        return lm;
     }
 
-    public void wifiError() { Log.d("RetrieveWifiLocation", "Wifi is not connected."); }
+    private void wifiError() { Log.d("RetrieveWifiLocation", "Wifi is not connected."); }
 }
