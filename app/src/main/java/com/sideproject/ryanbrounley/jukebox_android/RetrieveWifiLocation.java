74d777cefb74eaad701583e06eb243130ce411fc
Matthew Mayr <mmmayr@ucsc.edu>
2016-11-20 13:58:46 -0800

    Changed to make variables into variable functions.  getWifiName() returns function to help with player.

--- app/src/main/java/com/sideproject/ryanbrounley/jukebox_android/RetrieveWifiLocation.java
+++ app/src/main/java/com/sideproject/ryanbrounley/jukebox_android/RetrieveWifiLocation.java
@@ -2,35 +2,42 @@ package com.sideproject.ryanbrounley.jukebox_android;
 
 import android.content.Context;
 import android.location.LocationManager;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.util.Log;
 
 /**
  * Committed by Matthew Mayr on 11/3/2016.
  */
 
-class RetrieveWifiLocation {
+public class RetrieveWifiLocation {
 
     private Context lContext;
-    RetrieveWifiLocation(Context lContext) {
+    private LocationManager lm;
+    private WifiManager wm;
+    public RetrieveWifiLocation(Context lContext) {
         this.lContext = lContext;
+        lm = (LocationManager) lContext.getSystemService(Context.LOCATION_SERVICE);
+        wm = (WifiManager)lContext.getSystemService(Context.WIFI_SERVICE);
     }
-    LocationManager lm = (LocationManager) lContext.getSystemService(Context.LOCATION_SERVICE);
 
-    LocationManager getLocation() {
-        if (lm != null) {
-            boolean wifiEnabled = lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
-            //gets rough location from wifi name every 10 seconds or 10 meters
-            if (wifiEnabled) {
-                lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 10000L, 10F, (android.location.LocationListener) this);
-                return lm;
-            } else {
-                wifiError();
-            }
+    public boolean wifiEnabled(){
+        return lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+    }
+
+    public void getLocation() {
+        //gets rough location from wifi name every 10 seconds or 10 meters
+        if (wifiEnabled()) {
+            lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 10000L, 10F, (android.location.LocationListener) this);
         } else {
-            System.out.println ("Something went wrong with location manager.");
+            wifiError();
         }
-        return lm;
     }
 
-    private void wifiError() { Log.d("RetrieveWifiLocation", "Wifi is not connected."); }
+    public void wifiError() { Log.d("RetrieveWifiLocation", "Wifi is not connected."); }
+
+    public String getWifiName(){
+        WifiInfo wifiInfo = wm.getConnectionInfo();
+        return wifiInfo.getSSID();
+    }
 }
